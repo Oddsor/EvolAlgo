1018c55b8b89b1d583621f9ee3d9fa3532deb369
Andreas <andhag@stud.ntnu.no>
2013-02-28 22:04:34 +0100

    +WaveformDistance

---
+++ src/evolalgo/problem/sdm/WaveformDistance.java
@@ -0,0 +1,30 @@
+package evolalgo.problem.sdm;
+
+public class WaveformDistance implements ISDM {
+
+	/**
+	 * Class for computing the Waveform Distance Spike Train Distance Metrics
+	 * @author Andreas
+	 */
+	double p = 2.0;
+	@Override
+	public double calculateDistance(double[] target, double[] spiketrain) {
+		
+		int smallest = target.length < spiketrain.length ? target.length : spiketrain.length;
+		double sum = 0;
+		for (int i = 0; i < smallest; i++) {
+			
+			sum += Math.pow(Math.abs(spiketrain[i]-target[i]), p);
+			
+		}
+		
+		return sum/smallest;
+	}
+	public double getP() {
+		return p;
+	}
+	public void setP(double p) {
+		this.p = p;
+	}
+
+}
