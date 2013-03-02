5d7aad3e7edb7f3c5f4fa9578e8912bd2afe009e
Odd <Odd@Odd-PC>
2013-03-02 23:04:14 +0100

    added IGenotype for future assignments.

---
+++ src/evolalgo/IGenotype.java
@@ -0,0 +1,11 @@
+package evolalgo;
+
+/**
+ * Interface for genotypes, describes which functions the Evolutionary Algorithm
+ * requires to function.
+ * 
+ * @author Odd
+ */
+public interface IGenotype {
+    
+}
