f5db63a3fdd580165aa78840a44d423aadc941c6
Odd Andreas Sørsæther <osorseth@gmail.com>
2013-03-13 19:27:47 +0100

    "implementations" of the CTRNN phenotype and CTRNN problem
    Signed-off-by: Odd Andreas Sørsæther <osorseth@gmail.com>

---
+++ src/evolalgo/problem/ctrnn/CtrnnPhenotype.java
@@ -0,0 +1,12 @@
+
+package evolalgo.problem.ctrnn;
+
+import evolalgo.IPhenotype;
+
+/**
+ *
+ * @author Odd
+ */
+public class CtrnnPhenotype implements IPhenotype{
+    
+}
---
+++ src/evolalgo/problem/ctrnn/CtrnnProblem.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package evolalgo.problem.ctrnn;
+
+import evolalgo.IIndividual;
+import evolalgo.IndividualImpl;
+import evolalgo.problem.IProblem;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ *
+ * @author Odd
+ */
+public class CtrnnProblem implements IProblem{
+    
+    private int BIT_SIZE = 8;
+
+    @Override
+    public void developPheno(IIndividual individual) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void calculateFitness(List<IIndividual> population) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<IIndividual> createPopulation(int individuals) {
+        Random rand = new Random();
+        List<IIndividual> population = new ArrayList<IIndividual>();
+        for (int i = 0; i < individuals; i++){
+            String genome = "";
+            for (int j = 0; j < BIT_SIZE * 4; j++){
+                genome += rand.nextInt(2);
+            }
+            population.add(new IndividualImpl(genome));
+        }
+        return population;
+    }
+    
+}
